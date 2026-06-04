f1956020231a5ac0a53c9e6e191b1e6548260039
Éverton <evertonalencard24@gmail.com>
2026-06-04 18:15:04 -0300

    mais uma mudanca

--- Sources/ContaServiceArqui/configure.swift
+++ Sources/ContaServiceArqui/configure.swift
@@ -1,31 +1,49 @@
 import Vapor
 import Fluent
 import FluentPostgresDriver
+import NIOSSL
 
 public func configure(_ app: Application) async throws {
-    
-    app.migrations.add(CreateContaCorrente())
-    app.migrations.add(CreateContaCorrenteInternacional())
-    app.migrations.add(CreateContaPoupanca())
-    try await app.autoMigrate()
 
-    if let databaseURL = Environment.get("DATABASE_URL") {
-        try app.databases.use(.postgres(url: databaseURL), as: .psql)
-    } else {
-        let config = SQLPostgresConfiguration(
-            hostname: "localhost",
-            port: SQLPostgresConfiguration.ianaPortNumber,
-            username: "vapor",
-            password: "ContaService123",
-            database: "conta_service",
-            tls: .disable
-        )
+    if let databaseURL = Environment.get("DATABASE_URL"),
+       let url = URL(string: databaseURL) {
+
+        let username = url.user ?? "vapor"
+        let password = url.password ?? ""
+        let hostname = url.host ?? "localhost"
+        let port = url.port ?? 5432
+        let database = url.path.replacingOccurrences(of: "/", with: "")
+
         app.databases.use(
-            .postgres(configuration: config),
+            .postgres(
+                hostname: hostname,
+                port: port,
+                username: username,
+                password: password,
+                database: database,
+                tlsConfiguration: .forClient(certificateVerification: .none)
+            ),
+            as: .psql
+        )
+
+    } else {
+        app.databases.use(
+            .postgres(
+                hostname: "localhost",
+                port: 5432,
+                username: "vapor",
+                password: "ContaService123",
+                database: "conta_service",
+                tlsConfiguration: nil
+            ),
             as: .psql
         )
     }
 
-    
+    app.migrations.add(CreateContaCorrente())
+    app.migrations.add(CreateContaCorrenteInternacional())
+    app.migrations.add(CreateContaPoupanca())
+
+    try await app.autoMigrate()
     try routes(app)
 }
